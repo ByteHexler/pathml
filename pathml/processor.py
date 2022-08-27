6b0d0aa37e000e3e723cceab79fde24797723d5d
ByteHexler <92634454+ByteHexler@users.noreply.github.com>
2022-08-27 17:53:13 +0200

    update processor.py

--- pathml/processor.py
+++ pathml/processor.py
@@ -13,14 +13,14 @@ class Processor:
         self.__verbose = verbose
         self.__slideObject = slideObject
 
-    def applyModel(self, modelZip, batch_size, predictionKey = 'prediction', numWorkers=16):
+    def applyModel(self, modelZip, batch_size, predictionKey = 'prediction', numWorkers=16, tissueLevelThreshold=False, foregroundLevelThreshold=False, otsuLevelThreshold=False, triangleLevelThreshold=False, maskLevelThreshold=False):
         device, model, data_transforms = modelZip
-        pathSlideDataset = WholeSlideImageDataset(self.__slideObject, transform=data_transforms)
+        pathSlideDataset = WholeSlideImageDataset(self.__slideObject, transform=data_transforms, tissueLevelThreshold=tissueLevelThreshold, foregroundLevelThreshold=foregroundLevelThreshold, otsuLevelThreshold=otsuLevelThreshold, triangleLevelThreshold=triangleLevelThreshold, maskLevelThreshold=maskLevelThreshold)
         pathSlideDataloader = torch.utils.data.DataLoader(pathSlideDataset, batch_size=batch_size, shuffle=False, num_workers=numWorkers)
+        print(f"Processing {len(self.__slideObject.suitableTileAddresses(tissueLevelThreshold=tissueLevelThreshold, foregroundLevelThreshold=foregroundLevelThreshold, otsuLevelThreshold=otsuLevelThreshold, triangleLevelThreshold=triangleLevelThreshold, maskLevelThreshold=maskLevelThreshold))} of {len(self.__slideObject.suitableTileAddresses())} tiles...")
         for inputs in tqdm(pathSlideDataloader):
             inputTile = inputs['image'].to(device)
-            output = model(inputTile)
-            output = output.to(device)
+            output = model(inputTile).to(device)
 
             batch_prediction = torch.nn.functional.softmax(
                 output, dim=1).cpu().data.numpy()
@@ -30,7 +30,7 @@ class Processor:
                 tileAddress = (inputs['tileAddress'][0][index].item(),
                                inputs['tileAddress'][1][index].item())
                 self.__slideObject.appendTag(tileAddress, predictionKey, batch_prediction[index, ...])
-        return self
+        return self.__slideObject
 
     def adoptKeyFromTileDictionary(self, upsampleFactor=1):
         for orphanTileAddress in self.__slideObject.iterateTiles():
