c20536dceef7207210492737652c6d8cb625b524
ByteHexler <92634454+ByteHexler@users.noreply.github.com>
2022-08-27 17:57:40 +0200

    Update WholeSlideImageDataset.py

--- pathml/utils/torch/WholeSlideImageDataset.py
+++ pathml/utils/torch/WholeSlideImageDataset.py
@@ -6,29 +6,22 @@ from torch.utils.data import Dataset
 class WholeSlideImageDataset(Dataset):
     """WholeSlideImage dataset."""
 
-    def __init__(self, slideClass, foregroundOnly=False, tissueLevelThreshold=False, foregroundLevelThreshold=False, transform=None, segmenting=False):
+    def __init__(self, slideClass, tissueLevelThreshold=False, foregroundLevelThreshold=False, otsuLevelThreshold=False, triangleLevelThreshold=False, maskLevelThreshold=False, transform=None, segmenting=False):
         self.slideClass = slideClass
         self.tileSize = self.slideClass.tileSize
-        self.foregroundOnly = foregroundOnly
         self.tissueLevelThreshold = tissueLevelThreshold
         self.foregroundLevelThreshold = foregroundLevelThreshold
+        self.otsuLevelThreshold = otsuLevelThreshold
+        self.triangleLevelThreshold=triangleLevelThreshold
+        self.maskLevelThreshold = maskLevelThreshold
         self.transform = transform
         self.segmenting = segmenting
         self.suitableTileAddresses = self.slideClass.suitableTileAddresses(tissueLevelThreshold=self.tissueLevelThreshold,
-                                                                            foregroundLevelThreshold=self.foregroundLevelThreshold)
+                                                                            foregroundLevelThreshold=self.foregroundLevelThreshold,
+                                                                            otsuLevelThreshold=self.otsuLevelThreshold,
+                                                                            triangleLevelThreshold=self.triangleLevelThreshold,
+                                                                             maskLevelThreshold=self.maskLevelThreshold)
 
-        #for tA in self.slideClass.iterateTiles():
-        #    if self.tissueLevelThreshold and self.foregroundLevelThreshold:
-        #        if (self.slideClass.tileDictionary[tA]['tissueLevel'] >= self.tissueLevelThreshold) and (self.slideClass.tileDictionary[tA]['foregroundLevel'] <= self.foregroundLevelThreshold):
-        #            self.suitableTileAddresses.append(tA)
-        #    elif self.tissueLevelThreshold and not self.foregroundLevelThreshold:
-        #        if (self.slideClass.tileDictionary[tA]['tissueLevel'] >= self.tissueLevelThreshold):
-        #            self.suitableTileAddresses.append(tA)
-        #    elif self.foregroundLevelThreshold and not self.tissueLevelThreshold:
-        #        if (self.slideClass.tileDictionary[tA]['foregroundLevel'] <= self.foregroundLevelThreshold):
-        #            self.suitableTileAddresses.append(tA)
-        #    else:
-        #        self.suitableTileAddresses.append(tA)
 
     def __len__(self):
         #if self.tissueLevelThreshold or self.foregroundLevelThreshold:
@@ -39,17 +32,15 @@ class WholeSlideImageDataset(Dataset):
     # returns HWC 0-255 numpy array of tile if segmenting is False,
     # otherwise returns CHW 0-1 torch tensor. No transforms recommended for segmenting case
     def __getitem__(self, idx):
-        #if self.tissueLevelThreshold or self.foregroundLevelThreshold:
+
         tileAddress = self.suitableTileAddresses[idx]
-        #else:
-        #    tileAddress = self.slideClass.ind2sub(idx, foregroundOnly=self.foregroundOnly)
 
         if self.segmenting:
-            img = self.slideClass.getTile(tileAddress, writeToNumpy=True)
+            img = self.slideClass.getTile(tileAddress, writeToNumpy=True, useFetch=True)
             img = img[...,:3] # clip off the transparency channel
         else:
             img = Image.fromarray(self.slideClass.getTile(
-                tileAddress, writeToNumpy=True)).convert('RGB')
+                tileAddress, writeToNumpy=True, useFetch=True)).convert('RGB')
 
         if self.transform is not None:
             img = self.transform(img)
@@ -62,39 +53,5 @@ class WholeSlideImageDataset(Dataset):
             img = torch.from_numpy(img).type(torch.FloatTensor)
 
         sample = {'image': img, 'tileAddress': tileAddress}
-
         return sample
 
-
-# Used for Slide.inferSegmenter()
-'''
-class WholeSlideImageMaskDataset(Dataset):
-    """WholeSlideImageMask dataset."""
-
-    def __init__(self, slideClass, foregroundOnly=False, tissueLevelThreshold=False, foregroundLevelThreshold=False, transform=None):
-        self.slideClass = slideClass
-        self.foregroundOnly = foregroundOnly
-        self.tissueLevelThreshold = tissueLevelThreshold
-        self.foregroundLevelThreshold = foregroundLevelThreshold
-        self.transform = transform
-        self.suitableTileAddresses = self.slideClass.suitableTileAddresses(tissueLevelThreshold=self.tissueLevelThreshold,
-                                                                            foregroundLevelThreshold=self.foregroundLevelThreshold)
-
-    def __len__(self):
-        return len(self.suitableTileAddresses)
-
-    def __getitem__(self, idx):
-        tileAddress = self.suitableTileAddresses[idx]
-
-        img = Image.fromarray(self.slideClass.getTile(
-            tileAddress, writeToNumpy=True)).convert('RGB')
-
-        if self.transform is not None:
-            transformed = self.transform(image=img, mask=mask)
-            img = transformed['image']
-            mask = transformed['mask']
-
-        sample = {'image': img, 'tileAddress': tileAddress}
-
-        return sample
-'''
